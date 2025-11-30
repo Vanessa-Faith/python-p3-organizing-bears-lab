2945d0435510288db1946a381ba7f9d45c3f3292
Vanessa <vanessa.omondi@student.moringaschool.com>
2025-11-30 23:23:25 +0300

    message

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 15, "M", "brown", "calm", 1),
+("Rowdy", 8, "M", "black", "rowdy", 1),
+("Tabitha", 5, "F", "brown", "friendly", 1),
+("Sergeant Brown", 12, "M", "brown", "strict", 1),
+("Melissa", 10, "F", "white", "gentle", 1),
+("Grinch", 7, "M", "green", "grumpy", 1),
+("Wendy", 9, "F", "blonde", "cheerful", 1),
+(NULL, 6, "M", "gray", "mysterious", 1);
